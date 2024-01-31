256175f9aa97470e4d4bbbd3dabb04fe1cee1d35
Kanhaiyaa <128280779+Kanhaiyaa@users.noreply.github.com>
2024-01-31 17:03:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function volume_sphere() {
     //Write your code here
-  
+
+	if (isNaN(radius) || radius <=0) {
+		console.log("invalid radius")
+	}
+
+	let volume=(4/3)*Math.PI*Math.pow(radius,3)
+	return volume.toFixed(2)
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
